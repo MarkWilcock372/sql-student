d5b08559d80e1190ca213f132ff66d71e5bc5107
Mark Wilcock (personal account) <mark.wilcock@outlook.com>
2025-05-13 06:30:13 +0000

    ready for  adv course on 13 May 2025

--- advanced-lesson.sql
+++ advanced-lesson.sql
@@ -1,14 +1,2 @@
-SELECT
-    ps.PatientId
-    ,ps.AdmittedDate
-    ,ps.Hospital
-    , ps.Tariff
-    , (SELECT SUM(subq.Tariff) FROM PatientStay subq WHERE subq.PatientId <= ps.PatientId AND subq.Hospital = ps.Hospital) as CumulativeTariff
-    , (SELECT COUNT(*) FROM PatientStay subq WHERE subq.PatientId <= ps.PatientId AND subq.Hospital = ps.Hospital) As PatientIndex
-FROM
-    PatientStay ps
-ORDER BY ps.Hospital, ps.PatientId
-
-
-
+-- This SQL query counts the number of patients in each hospital
 SELECT Hospital, COUNT(*) FROM PatientStay GROUP by Hospital
